6c75a0e9717656fc80576c14b38374f234551de8
vincen <62093637+vincen-github@users.noreply.github.com>
2021-04-27 09:55:19 +0800

    Add files via upload

--- xgboost/BaseTree.py
+++ xgboost/BaseTree.py
@@ -22,7 +22,7 @@ class BaseTree(object):
     ===============
         1. max_depth: int, default=5
             Maximum depth of the tree
-        2. min_samples: int, default=5
+        2. min_samples_split: int, default=5
             The minimum number of samples in the leaf nodes of the tree.
         3. split_threshold: float, default=10
             The current point is split when the gain value of the optimal segmentation point is less than this value.
@@ -32,7 +32,9 @@ class BaseTree(object):
             Parameter used to select the best split feature and value and best value of loss function in a iteration.
         6. split_finding_strategy: {"exact", "approximate"}
             The strategy used in finding best split feature and value.
-        7. loss_func: function, default=LeastSquareLoss
+        7. sketch_eps: float, range=(0,1], default=0.1
+            we call sketch_eps is approximation factor.we use it to choose splitting candidates by following metric.
+        8. loss_func: function, default=LeastSquareLoss
             The function to calculate g and h in xgboost.
     Method:
     ========
@@ -61,6 +63,7 @@ class BaseTree(object):
                  gamma=1,
                  Lambda=1,
                  split_finding_strategy="exact",
+                 sketch_eps=0.1,
                  loss_func=LeastSquareLoss):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
@@ -68,6 +71,7 @@ class BaseTree(object):
         self.gamma = gamma
         self.Lambda = Lambda
         self.split_finding_strategy = split_finding_strategy
+        self.sketch_eps = sketch_eps
         self.loss_func = loss_func
 
     def train(self, X, y, depth=1):
@@ -99,7 +103,7 @@ class BaseTree(object):
             self.tree = -self.G / (self.H + self.Lambda)
             return self.tree
 
-        # if the numbers of samples in X less than min_samples
+        # if the numbers of samples in X less than min_samples_split
         # return the mean value of corresponding labels.
         if X.shape[0] < self.min_samples_split:
             logger.info("The number of samples in X less than min_samples_split({} < {}).".format(X.shape[0],
@@ -152,16 +156,57 @@ class BaseTree(object):
         for candidate_feature in self.feature_names:
             # set G_l = 0 and H_l = 0
             G_l, H_l = 0, 0
-            # iterate candidate value of candidate feature
-            for candidate_value in X[candidate_feature].drop_duplicates().sort_values(ascending=True):
-                G_l += X[X[candidate_feature] == candidate_value]["g"].sum()
-                H_l += X[X[candidate_feature] == candidate_value]["h"].sum()
-                G_r, H_r = self.G - G_l, self.H - H_l
-                # Prevent division by zero
-                if -self.Lambda not in (H_l, H_r, self.H):
-                    gain = (pow(G_l, 2) / (H_l + self.Lambda)) + (pow(G_r, 2) / (H_r + self.Lambda)) - (
-                            pow(self.G, 2) / (self.H + self.Lambda))
-                    gain_ls.append((candidate_feature, candidate_value, gain))
+            # if split_finding_strategy == 'exact'.we only need to sort the values of candidate feature.
+            if self.split_finding_strategy == 'exact':
+                candidate_values = X[candidate_feature].drop_duplicates().sort_values(ascending=True)
+                # iterate candidate value of candidate feature
+                for candidate_value in candidate_values:
+                    G_l += X[X[candidate_feature] == candidate_value]["g"].sum()
+                    H_l += X[X[candidate_feature] == candidate_value]["h"].sum()
+                    G_r, H_r = self.G - G_l, self.H - H_l
+                    # Prevent division by zero
+                    if -self.Lambda not in (H_l, H_r, self.H):
+                        gain = (pow(G_l, 2) / (H_l + self.Lambda)) + (pow(G_r, 2) / (H_r + self.Lambda)) - (
+                                pow(self.G, 2) / (self.H + self.Lambda))
+                        gain_ls.append((candidate_feature, candidate_value, gain))
+
+            # if split finding strategy is set as approximate.we need to find candidate values by following formula.
+            #                           |r_k(s_{k,j}) - r_k(s_{k,j+1)| < sketch_eps
+            if self.split_finding_strategy == 'approximate':
+                # initialize candidate_values = [min value of candidate feature],
+                # because s_{k1} = min_i{x_{ik}}, s_{kl} = max_i{x_{ik}} in xgboost paper.
+                candidate_values = [X[candidate_feature].min()]
+                # h_sum represent the value sum of h for all samples.
+                h_sum = X['h'].sum()
+                # r_k is to record the aggregate h values of x that x_{ik} in the interval [r_k(s_{k,j}), r_k(s_{k,j+1)]
+                r_k = 0
+                for index, row in X[[candidate_feature, 'h']] \
+                        .drop_duplicates() \
+                        .sort_values(by=candidate_feature, ascending=True).iterrows():
+                    # aggregate value of h, denoted that this r_k is different from r_k(z) in xgboost paper.
+                    r_k += row['h']
+                    # if |r_k(s_{k,j}) - r_k(s_{k,j+1)| > sketch_eps
+                    if r_k / h_sum > self.sketch_eps:
+                        # append x_{ik} to list and reset r_k as 0 to find the next candidate value.
+                        candidate_values.append(row[candidate_feature])
+                        r_k = 0
+                # if X[candidate_feature].max() is not contained in candidate_values
+                if X[candidate_feature].max() not in candidate_values:
+                    candidate_values.append(X[candidate_feature].max())
+
+                # iterate value in candidate values in above list obtained.
+                for i in range(len(candidate_values) - 1):
+                    # get rows of X satisfy the condition that r_k(s_{k,j}) < x_{ik} < r_k(s_{k,j+1)
+                    X_satisfy = X[candidate_values[i] <= X[candidate_feature]] \
+                        .query("{} <= {}".format(candidate_feature, candidate_values[i + 1]))
+                    G_l += X_satisfy["g"].sum()
+                    H_l += X_satisfy["h"].sum()
+                    G_r, H_r = self.G - G_l, self.H - H_l
+                    # Prevent division by zero
+                    if -self.Lambda not in (H_l, H_r, self.H):
+                        gain = (pow(G_l, 2) / (H_l + self.Lambda)) + (pow(G_r, 2) / (H_r + self.Lambda)) - (
+                                pow(self.G, 2) / (self.H + self.Lambda))
+                        gain_ls.append((candidate_feature, (candidate_values[i] + candidate_values[i + 1]) / 2, gain))
 
         # get the best feature and value from above gini_ls using compare the 3th element to find minimal variance
         best_index = 0
--- xgboost/XGBoost_test.py
+++ xgboost/XGBoost_test.py
@@ -2,6 +2,8 @@ from pandas import DataFrame
 from sklearn.datasets import load_boston
 import matplotlib.pyplot as plt
 
+from time import time
+
 from XGBModel import XGBModel
 from LeastSquareLoss import LeastSquareLoss
 
@@ -11,6 +13,39 @@ logging.basicConfig(level=logging.INFO,
                     format='%(levelname)s -  %(asctime)s - %(filename)s -  %(name)s - %(funcName)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
+def time_calc(func):
+    def wrapper(*args):
+        start_time = time()
+        res = func(*args)
+        end_time = time()
+        print("EXECUTION TIME:[{}]".format(end_time - start_time))
+        return res
+
+    return wrapper
+
+
+@time_calc
+def xgbTest(X, y):
+    xgb = XGBModel(n_estimator=10,
+                   gamma=1,
+                   Lambda=1,
+                   eta=0.3,
+                   max_depth=6,
+                   min_samples_split=3,
+                   split_threshold=20,
+                   sketch_eps=0.4,
+                   loss_func=LeastSquareLoss)
+
+    xgb.fit(X, y, split_finding_strategy="approximate")
+
+    y_pred = xgb.predict(X)
+
+    logger.info("MSE of xgbModel is [{}]".format(xgb.score(X, y)))
+
+    return y_pred
+
+
 if __name__ == "__main__":
     boston = load_boston()
 
@@ -18,27 +53,8 @@ if __name__ == "__main__":
 
     X = DataFrame(boston["data"], columns=boston["feature_names"])
     y = boston["target"]
-    # print(X)
-    # print(y)
 
-    # X['g'] = LeastSquareLoss.g(y, 0)
-    # X['h'] = LeastSquareLoss.h(y, 0)
-    # logger.info(X)
-
-    xgb = XGBModel(n_estimator=10,
-                   gamma=1,
-                   Lambda=1,
-                   eta=0.1,
-                   max_depth=5,
-                   min_samples_split=3,
-                   split_threshold=100,
-                   loss_func=LeastSquareLoss)
-
-    xgb.fit(X, y, split_finding_strategy="exact")
-
-    y_pred = xgb.predict(X)
-
-    logger.info(xgb.score(X, y))
+    y_pred = xgbTest(X, y)
 
     plt.figure(dpi=400)
     plt.plot(y, label="true")
